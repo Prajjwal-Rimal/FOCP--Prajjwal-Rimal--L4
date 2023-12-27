21ec36074bbac354f2746beb61a4c58305ce87eb
Prajjwal Rimal <rimalprajjwal1@gmail.com>
2023-12-27 18:25:20 +0545

    Final Submission
    Final Submission

---
+++ Portfolio/week 5/week5_5.py
@@ -0,0 +1,4 @@
+"""You wrote a program that processed a collection of temperature readings
+entered by the user and displayed the maximum, minimum, and mean. Create a
+version of that program that takes the values from the command-line instead. Be
+sure to handle the case where no arguments are provided!"""
