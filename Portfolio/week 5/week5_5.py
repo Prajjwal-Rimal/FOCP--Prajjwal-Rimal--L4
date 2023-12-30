1a29145162dcbba82cca8342827cfa988557b30b
Prajjwal Rimal <132150362+Prajjwal-Rimal@users.noreply.github.com>
2023-12-30 10:50:51 +0545

    Final Submission
    Final Submission

--- Portfolio/week 5/week5_5.py
+++ Portfolio/week 5/week5_5.py
@@ -2,3 +2,18 @@
 entered by the user and displayed the maximum, minimum, and mean. Create a
 version of that program that takes the values from the command-line instead. Be
 sure to handle the case where no arguments are provided!"""
+
+import sys
+
+def main():
+    a = [float(temperature) for temperature in sys.argv[1:]]
+    print('The maximum, minimum, and mean temperatures are:', calculate(a))
+
+def calculate(a):
+    max_temperature = max(a)
+    min_temperature = min(a)
+    mean_temperature = sum(a) / len(a)
+    return max_temperature, min_temperature, mean_temperature
+
+if __name__ == "__main__":
+    main()
