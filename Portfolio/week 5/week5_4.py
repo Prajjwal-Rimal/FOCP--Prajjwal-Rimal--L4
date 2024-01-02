aee5957cd5ce2bea2a635e32b10df7587cf5fbb7
Prajjwal Rimal <rimalprajjwal1@gmail.com>
2024-01-02 08:17:44 +0545

    Final Submission
    Final Submission

--- Portfolio/week 5/week5_4.py
+++ Portfolio/week 5/week5_4.py
@@ -2,3 +2,14 @@
 whether there is a working website at that address.
 Hint: You need to get the HTTP response code.
 Another Hint: StackOverflow is your friend"""
+
+import sys
+import requests
+
+tag = sys.argv[1]
+
+url = 'http://' + tag
+
+resp = requests.get(url)
+
+print(url)
--- Portfolio/week 5/week5_6.py
+++ Portfolio/week 5/week5_6.py
@@ -3,3 +3,15 @@ creates a backup copy of that file. The backup should contain an exact copy of t
 contents of the original and should, obviously, have a different name.
 Hint: By now, you should be getting the idea that there is a built-in way to do the
 heavy lifting here! Take a look at the "Brief Tour of the Standard Library" in the docs"""
+
+import sys
+
+f = open(sys.argv[1], 'r')
+f.seek(0)
+stri = f.read()
+print(stri)
+f.close()
+
+f = open("sample" + "1" + ".txt", 'w')
+f.write(stri)
+f.close()
